efefc3359edbd55d03c4160005cd62a338d86e0d
ZQZ44 <q.zhang-71@sms.ed.ac.uk>
2020-02-12 15:20:35 +0000

    Dataprovider update

--- Data_provider.py
+++ Data_provider.py
@@ -13,9 +13,9 @@ class VOCSegmentation(data.Dataset):
 
     def __init__(self, root, train=True, transform=None, target_transform=None, download=False, crop_size=None):
         self.root = os.path.expanduser(root)
-        _voc_root = os.path.join(self.root, 'VOC_trainval_aug')
-        _train_dir = os.path.join(_voc_root, 'JPEGImages')
-        _val_dir = os.path.join(_voc_root,'SementationClassAug')
+        _voc_root = self.root
+        _train_dir = os.path.join(_voc_root, 'VOCdevkit/VOC2012/JPEGImages/')
+        _val_dir = os.path.join(_voc_root,'SegmentationClassAug')
         self.transform = transform
         self.target_transform = target_transform
         self.train = train
@@ -32,21 +32,17 @@ class VOCSegmentation(data.Dataset):
         self.lables = []
         with open(_list_f, 'r') as lines:
             for line in lines:
-                image = os.path.join(_train_dir, line + ".jpg")
-                lable = os.path.join(_val_dir,line + ".png")
-                assert os.path.isfile(image)
-                assert os.path.isfile(lable)
+                image = os.path.join(_train_dir, line.rstrip('\n') + ".jpg")
+                lable = os.path.join(_val_dir,line.rstrip('\n') + ".png")
+                assert os.path.isfile(image),"no image in:" + image
+                assert os.path.isfile(lable),"no label in:" + lable
                 self.images.append(image)
                 self.lables.append(lable)
 
     def __getitem__(self, index):
         img = Image.open(self.images[index]).convert('RGB')
         target = Image.open(self.lables[index])
-        """
-        img, target = preprocess(img, target,
-                               flip=True if self.train else False,
-                               scale=(0.5, 2.0) if self.train else None,
-        """
+        
         if self.transform is not None:
             img = self.transform(img)
 
@@ -58,3 +54,23 @@ class VOCSegmentation(data.Dataset):
     def __len__(self):
         return len(self.images)
 
+if __name__ == '__main__':
+    from torch.utils.data import DataLoader
+    from torchvision import transforms
+    import matplotlib.pyplot as plt
+    
+    transform_train = trainsforms.Compse([
+          ])
+    voc_train = VOCSegmentation(root='./data',train=True)
+
+    dataloader = DataLoader(voc_train, batch_size=5, shuffle=True, num_workers=2)
+
+    for ii, img, target in enumerate(dataloader):
+            plt.figure()
+            plt.title('display')
+            plt.subplot(211)
+            plt.imshow(img)
+            plt.subplot(212)
+            plt.imshow(target)
+            break
+    plt.show(block=True)
